6994c65a5a522f6ffcb7bdfb290a388c60ce4c2b
Alejandro Piad <alepiad@gmail.com>
2014-01-16 17:27:34 -0500

    Changed the command name and added a few docs

--- pyeip.py
+++ pyeip.py
@@ -2,13 +2,13 @@ import sublime_plugin
 import math
 
 
-class PythonEvalInPlaceCommand(sublime_plugin.TextCommand):
+class InlinePythonCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for region in self.view.sel():
             if not region.empty():
                 # Get the selected text
                 s = self.view.substr(region)
-                # Transform it via rot13
+                # Evaluate the selected substring
                 s = repr(eval(s))
                 # Replace the selection with transformed text
                 self.view.replace(edit, region, s)
